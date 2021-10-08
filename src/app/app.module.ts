2527971f20325ea206ebf5f5f44ad4821f17ec71
carlos319822 <72896010+carlos319822@users.noreply.github.com>
2021-10-08 03:01:01 -0500

    primer clase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/utilidades/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
+import { SharedComponent } from './components/shared/shared.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeroesComponent,
+    AboutComponent,
+    SharedComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
